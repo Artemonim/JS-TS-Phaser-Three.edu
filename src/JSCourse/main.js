efd6069dfb9bc0ecb9e1cc9bd4c0a7decf2376d5
Artemonim <Artemonim@yandex.ru>
2025-02-17 14:43:17 +0300

    JS Course Task 3.7.3

--- src/JSCourse/main.js
+++ src/JSCourse/main.js
@@ -1,5 +1,7 @@
 // alert("Site started!")
 console.log("Site started!");
 
-const kek = "lol";
+const kek = Number.parseInt(" 10 или 20", 10);
 console.log(kek);
+
+console.log(+(5 * 5.5).toFixed(2));
