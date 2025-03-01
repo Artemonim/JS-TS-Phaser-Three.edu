fa20c8a39354a41db1f22ea98e86759d81211cf5
Artemonim <Artemonim@yandex.ru>
2025-03-01 10:37:05 +0300

    upd: JS Course Task 8.9.1

--- src/JSCourse/8.9.1.js
+++ src/JSCourse/8.9.1.js
@@ -2,12 +2,12 @@
 /* @returns {Array}
 */
 const arrayReverse = (arr) => {
-  const reversed = []
-  for (var i = arr.length-1; i >= 0; i--) {
-    reversed.push(arr.at(i))
+  const reversed = [];
+  for (let i = arr.length - 1; i >= 0; i--) {
+    reversed.push(arr.at(i));
   }
-  return reversed
+  return reversed;
 };
 
 // Должны получить массив: [105, 104, 103, 102, 101]
-console.log(arrayReverse([101, 102, 103, 104, 105]))
+console.log(arrayReverse([101, 102, 103, 104, 105]));
